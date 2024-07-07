b2fba67bce4571c8abf337718d81d9627de2525b
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-07 10:42:21 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+
+
+let cats = ["Milo","Otis","Garfield"]; 
+
+function destructivelyAppendCat(name) {
+  cats.push(name); 
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name); 
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop(); 
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift(); 
+}
+
+function appendCat(name) {
+  const newCats = [...cats, name]; 
+  return newCats;
+}
+
+function prependCat(name) {
+  const newCats = [name, ...cats];
+  return newCats;
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1); 
+}
+
+function removeFirstCat() {
+  return cats.slice(1); 
+}
+
+
